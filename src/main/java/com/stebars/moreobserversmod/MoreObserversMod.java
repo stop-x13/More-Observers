c40a435fe0977e7f89577c42f33bd8d6ad5fee0b
Stephen Barnes <stephen6174@gmail.com>
2021-09-24 17:54:17 -0500

    Rename to toggle observer

--- src/main/java/com/stebars/moreobserversmod/MoreObserversMod.java
+++ src/main/java/com/stebars/moreobserversmod/MoreObserversMod.java
@@ -1,7 +1,7 @@
 package com.stebars.moreobserversmod;
 
 import com.stebars.moreobserversmod.blocks.DiscernerBlock;
-import com.stebars.moreobserversmod.blocks.FlipFlopObserverBlock;
+import com.stebars.moreobserversmod.blocks.ToggleObserverBlock;
 
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
@@ -24,10 +24,10 @@ public class MoreObserversMod {
 	public static final Item DISCERNER_ITEM = new BlockItem(DISCERNER_BLOCK,
 			new Item.Properties().tab(ItemGroup.TAB_REDSTONE)).setRegistryName(new ResourceLocation(MOD_ID, "discerner"));
 
-	public static final Block FLIPFLOP_OBSERVER_BLOCK = new FlipFlopObserverBlock()
-			.setRegistryName(new ResourceLocation(MOD_ID, "flipflop_observer"));
-	public static final Item FLIPFLOP_OBSERVER_ITEM = new BlockItem(FLIPFLOP_OBSERVER_BLOCK,
-			new Item.Properties().tab(ItemGroup.TAB_REDSTONE)).setRegistryName(new ResourceLocation(MOD_ID, "flipflop_observer"));
+	public static final Block TOGGLE_OBSERVER_BLOCK = new ToggleObserverBlock()
+			.setRegistryName(new ResourceLocation(MOD_ID, "toggle_observer"));
+	public static final Item TOGGLE_OBSERVER_ITEM = new BlockItem(TOGGLE_OBSERVER_BLOCK,
+			new Item.Properties().tab(ItemGroup.TAB_REDSTONE)).setRegistryName(new ResourceLocation(MOD_ID, "toggle_observer"));
 	
 
 	public MoreObserversMod() {
@@ -37,16 +37,15 @@ public class MoreObserversMod {
 	@SubscribeEvent
 	public static void onBlocksRegistry(final RegistryEvent.Register<Block> event) {
 		event.getRegistry().registerAll(DISCERNER_BLOCK);
-		event.getRegistry().registerAll(FLIPFLOP_OBSERVER_BLOCK);
+		event.getRegistry().registerAll(TOGGLE_OBSERVER_BLOCK);
 	}
 
 	@SubscribeEvent
 	public static void onItemsRegistry(final RegistryEvent.Register<Item> event) {
 		event.getRegistry().registerAll(DISCERNER_ITEM);
-		event.getRegistry().registerAll(FLIPFLOP_OBSERVER_ITEM);
+		event.getRegistry().registerAll(TOGGLE_OBSERVER_ITEM);
 	}
 	
-	// TODO: observer variant that functions as a T-flip-flop, ie detects pulses, outputs constant
 	// TODO: observer variant that detects mobs in region (see Create Irrigation lava code). Will let you detect through walls
 	// TODO: observer variant that detects distance to first solid block
 	// TODO: observer that pulses for any changes in the 5-long line it's looking at (would need block entity to store it, I think?)
